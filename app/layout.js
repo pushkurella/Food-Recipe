0cd421746c06646b692219df0782bdbf83f81101
Pushpak <pushpak.kurella7@gmail.com>
2024-03-03 17:23:12 -0500

    add speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import MainHeader from "@/app/components/main-header/main-header";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "NextLevel Food",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       <body>
         <MainHeader />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
