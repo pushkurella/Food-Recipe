14d6b7f22745e4967e607aa99f190d44e8be4ec1
Pushpak <pushpak.kurella7@gmail.com>
2024-03-08 14:16:24 -0500

    update db

--- initdb.js
+++ initdb.js
@@ -1,5 +1,5 @@
-const sql = require("better-sqlite3");
-const db = sql("meals.db");
+// const sql = require("better-sqlite3");
+// const db = sql("meals.db");
 
 export const dummyMeals = [
   {
@@ -164,20 +164,20 @@ export const dummyMeals = [
   },
 ];
 
-db.prepare(
-  `
-   CREATE TABLE IF NOT EXISTS meals (
-       id INTEGER PRIMARY KEY AUTOINCREMENT,
-       slug TEXT NOT NULL UNIQUE,
-       title TEXT NOT NULL,
-       image TEXT NOT NULL,
-       summary TEXT NOT NULL,
-       instructions TEXT NOT NULL,
-       creator TEXT NOT NULL,
-       creator_email TEXT NOT NULL
-    )
-`
-).run();
+// db.prepare(
+//   `
+//    CREATE TABLE IF NOT EXISTS meals (
+//        id INTEGER PRIMARY KEY AUTOINCREMENT,
+//        slug TEXT NOT NULL UNIQUE,
+//        title TEXT NOT NULL,
+//        image TEXT NOT NULL,
+//        summary TEXT NOT NULL,
+//        instructions TEXT NOT NULL,
+//        creator TEXT NOT NULL,
+//        creator_email TEXT NOT NULL
+//     )
+// `
+// ).run();
 
 async function initData() {
   const stmt = db.prepare(`
